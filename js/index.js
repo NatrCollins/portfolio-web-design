a02b9085e3785d7f78ff8a16f8b6a89dcab780fa
Natalie <n.r.collins@outlook.com>
2021-07-10 00:15:45 +0930

    added JS and jQuery
    dark/ light mode function, hover states and shadow/illumination on buttons and cards

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 console.log("Well done you've loaded js");
+
+function toggleLight() {
+    if (document.documentElement.getAttribute("data-theme") === "light") {
+      document.documentElement.setAttribute("data-theme", "dark")
+      document.getElementById("btnToggleLight").innerHTML = "Light up my life"
+      console.log("setting theme to Dark")
+    } else {
+      document.documentElement.setAttribute("data-theme", "light")
+      document.getElementById("btnToggleLight").innerHTML = "I party at night"
+      console.log("setting theme to Light")
+    }
+  }
+  
+  // on button click, run toggleLight
+  
+  document.getElementById("btnToggleLight").addEventListener("click", toggleLight)
+
+$(".navMenu").hover(function(){
+    $(this).css("color","#C5B101").css("fontSize","1.3rem");
+}, function(){
+    $(this).css("color", "var(--textAccent)").css("fontSize","1.1rem");
+})
+
+
